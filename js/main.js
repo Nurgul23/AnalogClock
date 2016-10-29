2c30cc4fe30304daf022401f5171c86782c01a25
Aysel Emrahli <Aysel Emrahli>
2016-10-29 20:06:50 +0400

    fixed clock

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 ﻿$(document).ready(function() {
 	$('#clockCircle').children().css({"position": "absolute"})
-	$('#clockCircle').css({
-		"width": "600px",
-		"height": "600px",
-		"border": "3px solid purple",
-		"position": "relative",
-		"left": "350px"
-	})
+
 })	
 
 
@@ -19,7 +13,5 @@ setInterval(function(){
 $("#hour").css({"transform":"rotate("+hourDegree+"deg)"});
 $("#minute").css({"transform":"rotate("+minuteDegree+"deg)"});
 $("#seconds").css({"transform":"rotate("+secondDegree+"deg)"});
-
-
 
 	}, 1000)
